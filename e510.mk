ec459ab0e638c52f887bca376ad3a27101059de1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-01-03 16:21:48 +0000

    e510: Fix hw addresses for bcm4330
    Change-Id: I4ba136d8c9ae451251c5dbcb477e62e822269ddb

--- e510.mk
+++ e510.mk
@@ -75,6 +75,7 @@ $(call inherit-product, build/target/product/full.mk)
 PRODUCT_PACKAGES += \
     gralloc.e510 \
     gps.e510 \
+    hwaddrs \
     libOmxCore
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
